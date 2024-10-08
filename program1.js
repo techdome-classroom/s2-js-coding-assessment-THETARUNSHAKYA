4a3fca615aaf3a0cd63c823accfab65fc77f353a
Tarun shakya <tarun10128@gmail.com>
2024-10-08 14:51:59 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,8 +3,30 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
 };
+
+for (let char of s) {
+    if (char in mapping) {
+        let topElement = stack.length ? stack.pop() : '#';
+        if (mapping[char] !== topElement) {
+            return false;
+        }
+    } else {
+        stack.push(char);
+    }
+}
+
+return stack.length === 0;
+}
+
+console.log(isValid("()"));    
+console.log(isValid("()[]{}"));  
+console.log(isValid("(]"));       
 
 module.exports = { isValid };
 
